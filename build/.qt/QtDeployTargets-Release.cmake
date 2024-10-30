c1e869309833ba50e9459edb4b11baf30cdedd02
Jonathan Deininger <J_Deininger@web.de>
2024-10-30 20:35:19 +0100

    bla

--- build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_helloworld_FILE C:/_Code/c++qt/QtQuick-with-cmake/build/Release/helloworld.exe)
-set(__QT_DEPLOY_TARGET_helloworld_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_helloworld_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
